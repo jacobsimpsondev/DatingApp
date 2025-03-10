a81305dfe3d1c83c71c01f4996c7222b9b11366b
Jake Simpson <jacobsimpsondev@gmail.com>
2025-03-09 20:56:26 -0400

    Updates

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,25 +1,28 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-  private accountService = inject(AccountService);
-  loggedIn = false;
+  accountService = inject(AccountService);
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
       },
       error: error => console.error(error)
     })
+  }
+
+  logout() {
+    this.accountService.logout();
   }
 }
